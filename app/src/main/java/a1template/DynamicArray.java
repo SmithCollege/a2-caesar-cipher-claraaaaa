22e09ce86dce5482725f46c7f520fe6dcc40d8d3
LadanYang <124226825+LadanYang@users.noreply.github.com>
2025-02-13 18:56:49 +0000

    binah

--- app/src/main/java/a1template/DynamicArray.java
+++ app/src/main/java/a1template/DynamicArray.java
@@ -11,19 +11,28 @@ public class DynamicArray<T> implements IndexAccess<T>{
      * @param i index of element to read
      * @return value stored at the given index
      */
-    public T get(int i);
+    public T get(int i){
+        return contents[i];
+
+    };
 
     /** Returns the value stored at a given index plus the offset
      * @param i index of element to read
      * @param offset adjust index by this value
      * @return value stored at the given index
      */
-    public T get(int i, int offset);
+    public T get(int i, int offset){
+        return contents[i+offset];
+
+    };
 
     /** Stores the given value at the given index
      * @param i index of locaßtion to store
      * @param val value to store at given index
      */
-    public void set(int i, T val);
+    public void set(int i, T val){
+        contents[i]=val;
+
+    };
 
 }
